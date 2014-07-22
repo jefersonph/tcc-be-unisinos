9302f0ae9718ef7319396b36a617ff94e68f5ea0
jeferson Pizzolato Homem <jefersonph@gmail.com>
2014-07-22 14:47:16 -0300

    adicionando doencas

---
+++ app/views/diseases/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @disease, :id, :name, :user_id, :created_at, :updated_at
---
+++ db/migrate/20140722174637_create_diseases.rb
@@ -0,0 +1,10 @@
+class CreateDiseases < ActiveRecord::Migration
+  def change
+    create_table :diseases do |t|
+      t.string :name
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
